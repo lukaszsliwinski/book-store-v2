d08428a8fa998df9e6506fa9f1142b4f515859b7
lukaszliwinski <lukaszsliwinski0@gmail.com>
2022-12-18 17:36:48 +0100

    feat: create user model in db

--- backend/db/dbConnect.js
+++ backend/db/dbConnect.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 async function dbConnect() {
-    mongoose.set('strictQuery', true);
-    mongoose
-        .connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-        .then(() => console.log('connected to db'))
-        .catch((err) => {
-            console.log('unable to connect to db');
-            console.error(err);
-        });
+  mongoose.set('strictQuery', true);
+  mongoose
+    .connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log('connected to db'))
+    .catch((err) => {
+        console.log('unable to connect to db');
+        console.error(err);
+    });
 };
 
 module.exports = dbConnect;
