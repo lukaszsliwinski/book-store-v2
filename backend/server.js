5de3d0c4256cb35be3b8606d7e1c9bfa069ce8aa
lukaszliwinski <lukaszsliwinski0@gmail.com>
2022-12-18 19:30:18 +0100

    feat: create register endpoint

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@
 const path = require('path');
 const express = require('express');
 const bp = require('body-parser');
+const bcrypt = require('bcrypt');
 
 const dbConnect = require('./db/dbConnect');
+const User = require('./db/userModel');
 
 const app = express();
 
@@ -23,7 +25,39 @@ app.use(bp.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname, '../frontend/build')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
+});
+
+app.post('/register', (req, res) => {
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hashedPassword) => {
+      const user = new User({
+        username: req.body.username,
+        password: hashedPassword
+      });
+
+      user
+        .save()
+        .then((result) => {
+          res.status(201).send({
+            message: 'user successfully created',
+            result
+          });
+        })
+        .catch((err) => {
+          res.status(500).send({
+            message: 'error creating user',
+            err
+          });
+        });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: 'password was not hased successfully',
+        err
+      });
+    });
 });
 
 // execute database connection
@@ -31,5 +65,5 @@ dbConnect();
 
 // run server
 app.listen(PORT, () => {
-    console.log(`server listening on ${PORT}\nenv: ${NODE_ENV}`);
+  console.log(`server listening on ${PORT}\nenv: ${NODE_ENV}`);
 });
