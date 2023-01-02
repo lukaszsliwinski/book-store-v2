59dc6a9ea0cc286006f54f163abcd593435ec401
lukaszliwinski <lukaszsliwinski0@gmail.com>
2023-01-02 21:29:15 +0100

    feat: create simply navigation menu with logout function

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -19,11 +19,32 @@ export default function App() {
     if (token) setLogged(true);
   }, []);
 
+  const logout = () => {
+    cookies.remove("TOKEN", { path: "/" });
+    window.location.href = "/";
+  }
+
   return (
     <>
+      <nav>
+        <a href="/">home</a>
+        {!logged ? (
+          <>
+            <a href="/login">login</a>
+            <a href="/register">create an account</a>
+          </>
+        ) : (
+          <>
+            <span>logged as 'UserName'</span>
+            <button onClick={() => logout()}>logout</button>
+          </>
+
+        )}
+        <a href="/profile">profile</a>
+      </nav>
       <Routes>
       <Route path="/" element={<Home />}></Route>
-        <Route path="/login" element={<Login logged={logged} setLogged={setLogged}/>} />
+        <Route path="/login" element={<Login logged={logged} setLogged={setLogged} />} />
         <Route path="/register" element={<Register logged={logged} setLogged={setLogged} />} />
         <Route path="/profile" element={<ProtectedRoute component={<Profile />} />} />
       </Routes>
