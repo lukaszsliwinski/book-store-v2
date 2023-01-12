b85140dc586084dc9c679e0264c1ee63e60f5f69
Łukasz Śliwiński <74149405+lukaszsliwinski@users.noreply.github.com>
2023-01-12 13:55:43 +0100

    fix: correct req url in book details controller

--- backend/controllers/bookDetails.controller.js
+++ backend/controllers/bookDetails.controller.js
@@ -2,28 +2,23 @@ const axios = require('axios');
 
 const bookDetails = (request, response) => {
   axios
-    .get(`https://www.googleapis.com/books/v1/volumes?q=${request.body.id}&key=${process.env.API_KEY}&maxResults=1`)
+    .get(`https://www.googleapis.com/books/v1/volumes/${request.body.id}`)
     .then((result) => {
-      if (result.data.totalItems !== 0) {
-        let authors = [];
-        try {
-          result.data.items[0].volumeInfo.authors.map(author => authors.push(author));
-        } catch {};
+      let authors = [];
+      try {
+        result.data.volumeInfo.authors.map(author => authors.push(author));
+      } catch {};
 
-        response.json({
-          title: result.data.items[0].volumeInfo.title,
-          authors: authors,
-          description: (result.data.items[0].volumeInfo.description == undefined) ? '-' : result.data.items[0].volumeInfo.description,
-          publisher: (result.data.items[0].volumeInfo.publisher == undefined) ? '-' : result.data.items[0].volumeInfo.publisher,
-          publishedDate: (result.data.items[0].volumeInfo.publishedDate == undefined) ? '-' : result.data.items[0].volumeInfo.publishedDate,
-          price: (result.data.items[0].saleInfo.listPrice == undefined) ? 14.99 : result.data.items[0].saleInfo.listPrice.amount,
-          coverUrl: (result.data.items[0].volumeInfo.imageLinks == undefined) ? 'no-cover.png' : result.data.items[0].volumeInfo.imageLinks.thumbnail
-        });
-      } else {
-        response.json({
-          message: 'book not found'
-        });
-      };
+      response.json({
+        bookId: result.data.id,
+        title: result.data.volumeInfo.title,
+        authors: authors,
+        description: (result.data.volumeInfo.description == undefined) ? '-' : result.data.volumeInfo.description,
+        publisher: (result.data.volumeInfo.publisher == undefined) ? '-' : result.data.volumeInfo.publisher,
+        publishedDate: (result.data.volumeInfo.publishedDate == undefined) ? '-' : result.data.volumeInfo.publishedDate,
+        price: (result.data.saleInfo.listPrice == undefined) ? 14.99 : result.data.saleInfo.listPrice.amount,
+        coverUrl: (result.data.volumeInfo.imageLinks == undefined) ? 'no-cover.png' : result.data.volumeInfo.imageLinks.thumbnail
+      });
     })
     .catch((error) => {
       response.json({
