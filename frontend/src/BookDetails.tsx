637a2a2108aca1bc46cb6b65b8ff39ed123ff818
Łukasz Śliwiński <74149405+lukaszsliwinski@users.noreply.github.com>
2023-01-12 13:54:29 +0100

    feat: add book to cart from details page

--- frontend/src/BookDetails.tsx
+++ frontend/src/BookDetails.tsx
@@ -2,12 +2,16 @@ import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
-import { IBookData } from './types';
+import { IBookData, IBookInCart } from './types';
+import { addToCart } from './utils';
 
 export default function BookDetails() {
   const [bookData, setBookData] = useState<IBookData>();
   const [coverUrl, setCoverUrl] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
+  const [dataToCart, setDataToCart] = useState<IBookInCart>();
+  const [counter, setCounter] = useState(1);
+
   const params = useParams();
 
   useEffect(() => {
@@ -32,6 +36,14 @@ export default function BookDetails() {
           } else {
             setCoverUrl(result.data.coverUrl);
           };
+
+          setDataToCart({
+            bookId: result.data.bookId,
+            title: result.data.title,
+            authors: result.data.authors,
+            price: result.data.price,
+            amount: counter
+          });
         };
       })
       .catch((error) => {
@@ -40,6 +52,17 @@ export default function BookDetails() {
       });
   }, []);
 
+  useEffect(() => {
+    if (dataToCart) {
+      setDataToCart({
+        bookId: dataToCart.bookId,
+        title: dataToCart.title,
+        authors: dataToCart.authors,
+        price: dataToCart.price,
+        amount: counter
+      });
+    };
+  }, [counter]);
 
   return (
     <>
@@ -52,6 +75,12 @@ export default function BookDetails() {
         <div>Price: {bookData.price} $</div>
         <div>Publisher: {bookData.publisher}</div>
         <div>Published date: {bookData.publishedDate}</div>
+        <button onClick={() => {if (dataToCart) addToCart(dataToCart)}}>add to cart</button>
+          <div>
+            <button onClick={() => { if (counter > 1) setCounter(counter - 1) }}>-</button>
+            <label>{counter}</label>
+            <button onClick={() => { if (counter < 5) setCounter(counter + 1) }}>+</button>
+          </div>
       </div> :
       <div>{errorMessage}</div>}
     </>
