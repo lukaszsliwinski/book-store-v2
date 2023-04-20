37c0a3a8dde4faf98124cf4b75e0ac84ea6fcc53
lukaszliwinski <lukaszsliwinski0@gmail.com>
2023-04-20 21:38:17 +0200

    feat: show order details after clicking on the header

--- frontend/src/components/HistoryItem.tsx
+++ frontend/src/components/HistoryItem.tsx
@@ -4,23 +4,21 @@ import { IOrder } from '../types';
 export default function HistoryItem({ order }: { order: IOrder }) {
   return (
     <div className="dark:shadow-dark my-2 rounded-sm bg-white p-2 shadow-md dark:bg-zinc-900">
-      <div className="flex items-center justify-between text-xs">
+      <button
+        className="flex w-full items-center justify-between text-xs hover:text-teal-700"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target={`#collapse${order.number}`}
+        aria-expanded="false"
+        aria-controls={`collapse${order.number}`}
+      >
         <span className="font-bold">
           order {order.number} - {order.date.substring(0, 10)}
         </span>
-        <span></span>
-
-        <button
-          className="p-1 hover:text-teal-700"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target={`#collapse${order.number}`}
-          aria-expanded="false"
-          aria-controls={`collapse${order.number}`}
-        >
+        <span className="p-1">
           <ArrowDown className="ml-1 w-2" />
-        </button>
-      </div>
+        </span>
+      </button>
 
       <div className="collapse border-t" id={`collapse${order.number}`}>
         <div className="p-2 pb-0">
