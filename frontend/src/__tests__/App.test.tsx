c06948f75a4a49f227dddcf91481628478b31622
Łukasz Śliwiński <74149405+lukaszsliwinski@users.noreply.github.com>
2023-06-29 15:20:40 +0200

    docs: mistake in coment

--- frontend/src/__tests__/App.test.tsx
+++ frontend/src/__tests__/App.test.tsx
@@ -6,7 +6,7 @@ describe('Rendering', () => {
   test('Render home page', async () => {
     setup('/');
 
-    // check if logo and search buttom are in the document
+    // check if logo and search button are in the document
     expect(await screen.findByText(/book store/)).toBeInTheDocument();
     expect(await screen.findAllByText(/search/)).toHaveLength(2);
   });
