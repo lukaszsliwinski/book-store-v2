0694f8391a3cfa696a96668f236e34b2db4b79d3
lukaszliwinski <lukaszsliwinski0@gmail.com>
2023-11-13 18:35:23 +0100

    config: update tailwind config file

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   darkMode: 'class',
   content: ['./src/**/*.{js,jsx,ts,tsx}', './node_modules/tw-elements/dist/js/**/*.js'],
   theme: {
+    screens: {
+      'xs': '480px',
+      ...defaultTheme.screens
+    },
     extend: {
       colors: {
         zinc: {
@@ -11,9 +18,6 @@ module.exports = {
         neutral: {
           50: '#fafafa'
         }
-      },
-      screens: {
-        xs: '480px'
       },
       boxShadow: {
         dark: '0 4px 6px -1px rgb(0 0 0 / 0.25), 0 2px 4px -2px rgb(0 0 0 / 0.25)'
